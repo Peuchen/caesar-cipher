6a54a404f33425d34a49aeb9f9bc6a01344c19e7
peuchen <npeuchen@gmail.com>
2020-09-25 21:27:33 +0200

    Refactor input and output

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,40 +1,29 @@
 puts "Please enter a string to encrypt"
-string = gets.chomp
+string = gets.chomp.split('')
 
 puts "Please enter a shift factor"
 shift_factor = gets.chomp.to_i
-
-val = string.split('')
-
 if shift_factor > 26 || shift_factor < -26
   shift_factor %= 26
 end
 
-def convert_characters(arr, shift_factor)
-  output = arr.map { |c| c.ord}
-  output = output.map do |n|
-    if n >= "A".ord && n <= "Z".ord
-      n += shift_factor
-      if n > "Z".ord
-        n -= 26
-      elsif n < "A".ord
-        n += 26
-      end
-    elsif n >= "a".ord && n <= "z".ord
-      n += shift_factor
-      if n > "z".ord
-        n -= 26
-      elsif n < "a".ord
-        n += 26
-      end
+output = string.map do |character| n = character.ord
+  if n >= "A".ord && n <= "Z".ord
+    n += shift_factor
+    if n > "Z".ord
+      n -= 26
+    elsif n < "A".ord
+      n += 26
     end
-    n
+  elsif n >= "a".ord && n <= "z".ord
+    n += shift_factor
+    if n > "z".ord
+      n -= 26
+    elsif n < "a".ord
+      n += 26
+    end
   end
-
-  output = output.map { |n| n.chr }
-  output = output.join('')
-  puts "The encrypted text is:"
-  puts output
+  n.chr
 end
 
-convert_characters(val, shift_factor)
+puts "The encrypted text is:\n#{output.join('')}"
