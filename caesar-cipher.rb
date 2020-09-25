9dfa121d19c306a860334d968781c2418f1d4e63
peuchen <npeuchen@gmail.com>
2020-09-25 15:39:22 +0200

    Add simplify_shift method

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -9,16 +9,24 @@ puts "You have entered the shift factor'#{shift_factor}'"
 #Split the sentence in separate characters
 values = string.split('')
 p values
+
+#If the shift factor exceeds 26 or -26, detract (mutiples of) 26 to keep the shift factor within the range
+def simplify_shift(shift_factor)
+  if shift_factor > 26 || shift_factor < -26
+    shift_factor %= 26
+  end
+end
+
 #Convert the characters into numbers
 convert_characters(values)
 
-def convert_characters(arr)
+def convert_characters(arr, shift_factor)
   output = arr.map { |c| c.ord }
   p output
-end
 
-#Add the shift factor to the numbers
 #If the new number exceeds the range for the characters, add or detract (multiples of) 26 to keep it within the range
+#Add the shift factor to the numbers
+end
 #Convert the numbers back into characters
 #Join the characters back together
 #Print the string
