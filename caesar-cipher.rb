08051884a52622892edaecd14f274ffa68a6a23b
peuchen <npeuchen@gmail.com>
2020-09-25 14:37:06 +0200

    Create file containing pseudocode

---
+++ caesar-cipher.rb
@@ -0,0 +1,9 @@
+#Ask the user to input a string
+#Ask the user to choose a shift factor
+#Split the sentence in separate characters
+#Convert the characters into numbers
+#Add the shift factor to the numbers
+#If the new number exceeds the range for the characters, add or detract (multiples of) 26 to keep it within the range
+#Convert the numbers back into characters
+#Join the characters back together
+#Print the string
