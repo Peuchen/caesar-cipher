f59d2054f53eb3754c203d9322cc376c86955bd2
peuchen <npeuchen@gmail.com>
2020-09-25 15:26:33 +0200

    Split the sentence and convert characters to numbers

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,9 +1,22 @@
 #Ask the user to input a string
 string = gets.chomp
+puts "You have entered the string '#{string}'"
+
 #Ask the user to choose a shift factor
 shift_factor = gets.chomp.to_i
+puts "You have entered the shift factor'#{shift_factor}'"
+
 #Split the sentence in separate characters
+values = string.split('')
+p values
 #Convert the characters into numbers
+convert_characters(values)
+
+def convert_characters(arr)
+  output = arr.map { |c| c.ord }
+  p output
+end
+
 #Add the shift factor to the numbers
 #If the new number exceeds the range for the characters, add or detract (multiples of) 26 to keep it within the range
 #Convert the numbers back into characters
